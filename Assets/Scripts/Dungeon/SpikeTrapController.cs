ef78ee46c23b6be9f89b094ed4d980d3a534a21f
SoKH-creator <skh7235@naver.com>
2025-08-04 11:14:17 +0900

    Reapply "[feat] 가시 함정 프리팹 애니메이션, 데미지 구현"
    This reverts commit 6cf20bcf4896621b3eff0abaf1fd9851c97b520d.

---
+++ Assets/Scripts/Dungeon/SpikeTrapController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrapController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Collider2D spikeCollider;
+    [SerializeField] private float delay = 1.5f;
+    [SerializeField] private float damage = 2.0f;
+    [SerializeField] private float damageCooldowns = 1.0f;
+
+    private Dictionary<Player, float> lastHitTime = new();
+        
+
+    private void Start()
+    {
+        StartCoroutine(SpikeRoutione());
+    }
+
+    private IEnumerator SpikeRoutione()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+
+            // start animation
+            animator.SetTrigger("IsActive");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Health -= damage;
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null && spikeCollider.enabled)
+            {
+                lastHitTime.TryGetValue(player, out lastHitTime);
+
+                if (Time.time - lastHitTime >= damageCooldowns)
+                {
+                    player.Health -= damage;
+                    lastHitTime[player] = Time.time;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            if (player != null && lastHitTime.ContainsKey(player))
+            {
+                lastHitTime.Remove(player); // remove record when out of range
+            }
+        }
+    }
+}
